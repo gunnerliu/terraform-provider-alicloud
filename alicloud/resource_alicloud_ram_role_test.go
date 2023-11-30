666dd0bee827c985a3db303d3d30e3049240e4cc
guimin.hgm <guimin.hgm@alibaba-inc.com>
2023-11-30 14:24:19 +0800

    testcase: Improves the alicloud_ram_policy sweeper test

--- alicloud/resource_alicloud_ram_role_test.go
+++ alicloud/resource_alicloud_ram_role_test.go
@@ -2,17 +2,16 @@ package alicloud
 
 import (
 	"fmt"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/requests"
 	"log"
 	"testing"
-
-	"strings"
-	"time"
 
 	"github.com/aliyun/alibaba-cloud-sdk-go/services/ram"
 	"github.com/aliyun/terraform-provider-alicloud/alicloud/connectivity"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"strings"
 )
 
 func init() {
@@ -38,73 +37,74 @@ func testSweepRamRoles(region string) error {
 		"tf_testAcc",
 	}
 
-	request := ram.CreateListRolesRequest()
-	raw, err := client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
-		return ramClient.ListRoles(request)
-	})
-	if err != nil {
-		return WrapError(err)
-	}
-	resp, _ := raw.(*ram.ListRolesResponse)
-	if len(resp.Roles.Role) < 1 {
-		return nil
-	}
-
-	sweeped := false
-
-	for _, v := range resp.Roles.Role {
-		name := v.RoleName
-		id := v.RoleId
-		skip := true
-		for _, prefix := range prefixes {
-			if strings.HasPrefix(strings.ToLower(name), strings.ToLower(prefix)) {
-				skip = false
-				break
-			}
-		}
-		if skip {
-			log.Printf("[INFO] Skipping Ram Role: %s (%s)", name, id)
-			continue
-		}
-		sweeped = true
-
-		log.Printf("[INFO] Detaching Ram Role: %s (%s) policies.", name, id)
+	for {
+		request := ram.CreateListRolesRequest()
+		request.MaxItems = requests.NewInteger(100)
 		raw, err := client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
-			request := ram.CreateListPoliciesForRoleRequest()
-			request.RoleName = name
-			return ramClient.ListPoliciesForRole(request)
+			return ramClient.ListRoles(request)
 		})
-		resp, _ := raw.(*ram.ListPoliciesForRoleResponse)
 		if err != nil {
-			log.Printf("[ERROR] Failed to list Ram Role (%s (%s)) policies: %s", name, id, err)
-		} else if len(resp.Policies.Policy) > 0 {
-			for _, v := range resp.Policies.Policy {
-				_, err := client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
-					request := ram.CreateDetachPolicyFromRoleRequest()
-					request.PolicyName = v.PolicyName
-					request.RoleName = name
-					request.PolicyType = v.PolicyType
-					return ramClient.DetachPolicyFromRole(request)
-				})
-				if err != nil && !IsExpectedErrors(err, []string{"EntityNotExist"}) {
-					log.Printf("[ERROR] Failed detach Policy %s: %#v", v.PolicyName, err)
+			return WrapError(err)
+		}
+		resp, _ := raw.(*ram.ListRolesResponse)
+		if len(resp.Roles.Role) < 1 {
+			return nil
+		}
+
+		for _, v := range resp.Roles.Role {
+			name := v.RoleName
+			id := v.RoleId
+			skip := true
+			for _, prefix := range prefixes {
+				if strings.HasPrefix(strings.ToLower(name), strings.ToLower(prefix)) {
+					skip = false
+					break
 				}
 			}
-		}
+			if skip {
+				log.Printf("[INFO] Skipping Ram Role: %s (%s)", name, id)
+				continue
+			}
 
-		log.Printf("[INFO] Deleting Ram Role: %s (%s)", name, id)
-		request := ram.CreateDeleteRoleRequest()
-		request.RoleName = name
+			log.Printf("[INFO] Detaching Ram Role: %s (%s) policies.", name, id)
+			raw, err := client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
+				request := ram.CreateListPoliciesForRoleRequest()
+				request.RoleName = name
+				return ramClient.ListPoliciesForRole(request)
+			})
+			resp, _ := raw.(*ram.ListPoliciesForRoleResponse)
+			if err != nil {
+				log.Printf("[ERROR] Failed to list Ram Role (%s (%s)) policies: %s", name, id, err)
+			} else if len(resp.Policies.Policy) > 0 {
+				for _, v := range resp.Policies.Policy {
+					_, err := client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
+						request := ram.CreateDetachPolicyFromRoleRequest()
+						request.PolicyName = v.PolicyName
+						request.RoleName = name
+						request.PolicyType = v.PolicyType
+						return ramClient.DetachPolicyFromRole(request)
+					})
+					if err != nil && !IsExpectedErrors(err, []string{"EntityNotExist"}) {
+						log.Printf("[ERROR] Failed detach Policy %s: %#v", v.PolicyName, err)
+					}
+				}
+			}
 
-		_, err = client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
-			return ramClient.DeleteRole(request)
-		})
-		if err != nil {
-			log.Printf("[ERROR] Failed to delete Ram Role (%s (%s)): %s", name, id, err)
+			log.Printf("[INFO] Deleting Ram Role: %s (%s)", name, id)
+			request := ram.CreateDeleteRoleRequest()
+			request.RoleName = name
+
+			_, err = client.WithRamClient(func(ramClient *ram.Client) (interface{}, error) {
+				return ramClient.DeleteRole(request)
+			})
+			if err != nil {
+				log.Printf("[ERROR] Failed to delete Ram Role (%s (%s)): %s", name, id, err)
+			}
 		}
-	}
-	if sweeped {
-		time.Sleep(5 * time.Second)
+		if !resp.IsTruncated {
+			break
+		}
+		request.Marker = resp.Marker
 	}
 	return nil
 }
